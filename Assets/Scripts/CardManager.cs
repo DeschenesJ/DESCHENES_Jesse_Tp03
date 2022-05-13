61c05ea24025418f0c34965a121c8451995a6365
Jesse Deschênes <1706855@cegepat.qc.ca>
2022-05-13 11:28:11 -0400

    création d'un card manager
    Commit pour la création d'un card manager qui va s'occuper de gérer les cartes

--- Assets/Scripts/CardManager.cs
+++ Assets/Scripts/CardManager.cs
@@ -11,20 +11,32 @@ public class CardManager : MonoBehaviour
     // Va chercher le frame physique de la carte
     public GameObject CardFrame;
 
-    public Carte strike;
+    public Carte strikeCard;
+    public Carte guardCard;
+    public Carte healCard;
 
-    private List<Carte> lstCardType;
-    private List<Carte> lstAllCards;
-    private List<Carte> lstdeckZone;
-    private List<Carte> lsthandZone;
-    private List<Carte> lstdiscardZone;
+    // Les listes pour les différentes zones
+    private List<Carte> lstAllCards; // Liste des cartes du joueur peu importe la zone
+    private List<Carte> lstdeckZone; // Liste des cartes encore non pigés dans le deck du joueur
+    private List<Carte> lsthandZone; // Liste des cartes dans la main que le joueur peut jouer
+    private List<Carte> lstdiscardZone; // Liste des cartes que le joueur a utilisées
 
-    
+    //Variable pour la variable qui détermine la taille du deck de départ
+    private int startingDeck = 15;
+
+    // Variable qui mélange les cartes du deck du joueur et lorsque les cartes retourne dans ce dernier
+    private bool isDeckShuffle;
+
+    // Variable qui détermine s'il reste des cartes dans le deck du joueur au début de son tour.
+    private bool isDeckEmpty;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+
+
+
+
     }
 
     // Update is called once per frame
